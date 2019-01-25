557c7801d0ca766c14d57ef42d897ed31411a4eb
Jesus Lanchas <jesus.lanchas@daureos.com>
2019-01-25 19:51:41 +0100

    feat: add style to pending TX messages

--- src/tx/ui/expectingConfirmations/ExpectingConfirmations.js
+++ src/tx/ui/expectingConfirmations/ExpectingConfirmations.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Segment, Header, List, Loader } from 'semantic-ui-react';
 
 export const CONFIRMATIONS_THRESHOLD = 1;
 
@@ -16,20 +17,26 @@ class ExpectingConfirmations extends Component {
         });
 
         return (
-            <span>
-                Waiting for [{pendingTx.length}] transactions.
-                <ul>
+            <Segment raised>
+                <Header><Loader size='small' active inline /> Waiting for [{pendingTx.length}] transactions</Header>
+                <List divided relaxed>
                     {pendingTx.map(tx => (
                         <PendingTx key={tx.txHash} {...tx} />
                     ))}
-                </ul>
-            </span>
+                </List>
+            </Segment>
         );
     }
 }
 
 const PendingTx = ({ txHash, expectedConfirmations }) => (
-  <li>Waiting {expectedConfirmations} more confirmations for TX [{txHash}]</li>
+    <List.Item key={txHash}>
+        <List.Icon name='ethereum' size='large' verticalAlign='middle' />
+        <List.Content>
+            <List.Header as='a' target='_blank' href={'https://rinkeby.etherscan.io/tx/' + txHash}>{txHash}</List.Header>
+            <List.Description as='a'>Waiting for {expectedConfirmations} confirmations yet</List.Description>
+        </List.Content>
+    </List.Item>
 )
 
 export default ExpectingConfirmations;
